80d0c6b8e98b06ae062fd16831e7448c9a8dd06d
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2019-07-13 13:59:42 +0200

    added testcase on library user level

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,4 +1,26 @@
 #[cfg(test)]
 mod tests {
-    use serde_meta_derive::SerdeMeta;
+    use serde_meta_derive;
+    use serde_meta::*;
+
+    mod test_derive_empty_struct {
+        use super::*;
+
+        #[derive(serde_meta_derive::SerdeMeta)]
+        struct A {}
+
+        #[test]
+        fn test() {
+            let meta = A::meta();
+            if let TypeInformation::StructValue{name, fields} = meta {
+                assert_eq!("A", name);
+                let expected_fields: &[Field] = &[];
+                assert_eq!(expected_fields, fields);
+            } else {
+                panic!("Expected StructValue, but got {:#?}",
+                       meta
+                );
+            }
+        }
+    }
 }
